da0347bf5adf97ee858bdb6d3c126463ccfc336f
Nutthakit Anansuksakul <ing118199@gmail.com>
2021-05-06 21:18:02 +0700

    first commit

---
+++ config/connect_sample.php
@@ -0,0 +1,11 @@
+//file นี้ ไม่เอาไว้ใน git จะใช้ ignore จะไปถูกเอาไปเก็บไว้ใน git
+<?php
+$host = "";
+$user = "";
+$password = "";
+$db = "";
+
+mysqli_connect($host,$user,$password,$dbname);
+echo "this is a connecting database file";
+
+
